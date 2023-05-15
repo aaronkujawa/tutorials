e2a06f385955a48fcf91f833bf3fa630ec102cc7
Aaron Kujawa <askujawa@gmail.com>
2023-05-15 09:12:40 +0000

    Added install_requirements.sh

---
+++ modules/dynunet_pipeline/install_requirements.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip install torch --upgrade
+pip install pytorch-ignite nibabel tqdm 
+pip install git+https://github.com/aaronkujawa/MONAI.git@medtronic 
+pip install git+https://github.com/MIC-DKFZ/HD-BET 
+
